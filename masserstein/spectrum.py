aba19e89d17e0e22e68a37f4b54133144a86b799
Leonhard Lücken <leonhard.luecken@uol.de>
2025-04-23 15:25:55 +0200

    fixed import

--- masserstein/spectrum.py
+++ masserstein/spectrum.py
@@ -8,7 +8,7 @@ import re
 from collections import Counter
 import numpy.random as rd
 from scipy.signal import argrelmax
-from .peptides import get_protein_formula
+from masserstein.peptides import get_protein_formula
 from warnings import warn
 from copy import deepcopy
 
